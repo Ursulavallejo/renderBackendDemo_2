589414d1a0f18e40b289becae1a5ca72ab48d165
Ursula Vallejo Janne <ursulavallejo@gmail.com>
2024-10-24 12:31:23 +0200

    update2

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ app.use(cors())
 app.get('/products/:id', function (req, res, next) {
   res.json({ msg: 'This is CORS-enabled for all origins!' })
 })
+app.get('/', (_request, response) => {
+  response.send({ hello: 'World' })
+})
 
 app.listen(80, function () {
   console.log('CORS-enabled web server listening on port 80')
